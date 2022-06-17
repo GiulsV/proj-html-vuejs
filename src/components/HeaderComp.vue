18244e1e83185a54c0300ccec6e86658723af20f
GiuliaV <valente.giulia93@gmail.com>
2022-06-17 19:19:38 +0200

    Header - modificato layout (da sistemare)

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -2,12 +2,13 @@
 <!-- Header -->
 <header>
     <!-- Section countdown -->
+    <section class="container-fluid d-flex flex-row justify-content-center" id="countdown">
+        <p class="align-self-center"> Start tomorrow our biggest event of the year <i class="far fa-clock"></i> 00:00:00:00 <button>GET ticket</button></p>
+        
+    </section>
 
     <!-- Header Navigazione Dinamico-->
-    <div class="container">
-        <NavBar />      
-
-    </div>
+    <NavBar />
 
 </header>
 <!-- Fine Header -->
@@ -25,5 +26,8 @@ export default{
 </script>
 
 <style lang="scss" scoped>
+#countdown{
+    background-color: #F4F0EF;
+}
 
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,87 +1,103 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-white text-dark">
+  <div class="container d-flex flex-column flex-nowrap">
+    <!-- <div class="row"> -->
+      <!-- <div class="col-xs-12"> -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-white text-dark justify-content-between">
         <!-- Logo a sinistra-->
-        <a class="navbar-brand">
-            <img src="../assets/images/logo/dark-logo.png" alt="Logo">
-        </a>
-        <!-- Navbar in centro-->
-        <div>
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item mx-0 mx-lg-1"
-                    v-for="(link, index) in navLink"
-                    :key="index">
-                    <a :href="link.src" class="nav-link py-3 px-0 px-lg-3 text-dark">
-                        {{link.label}}<i class="fas fa-chevron-down"></i>
-                    </a>
-                </li>
-            </ul>
-            <!-- Icone social a destra -->
-            <ul class="navbar-nav mb-2 mb-lg-0">
-                <li class="nav-item mx-0 mx-lg-1">
-                    <a class="nav-link py-3 px-0 px-lg-3 text-dark" href="#">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                </li>
-                <li class="nav-item mx-0 mx-lg-1">
-                    <a class="nav-link py-3 px-0 px-lg-3 text-dark" href="#">
-                        <i class="fab fa-facebook-f"></i>
-                    </a>
-                </li>
-                <li class="nav-item mx-0 mx-lg-1">
-                    <a class="nav-link py-3 px-0 px-lg-3 text-dark" href="#">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                </li>
-                <li class="nav-item mx-0 mx-lg-1">
-                    <a class="nav-link py-3 px-0 px-lg-3 text-dark" href="#">
-                        <i class="fab fa-linkedin"></i>
-                    </a>
-                </li>
-            </ul>
+        <div class="logo d-flex flex-row justify-content-center">
+          <a class="navbar-brand">
+            <img src="../assets/images/logo/dark-logo.png" alt="Logo" />
+          </a>
         </div>
 
-    </nav>
+        <!-- Navbar in centro-->
+        
+          <div class="d-flex flex-row justify-content-center">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li
+                class="nav-item mx-0 mx-lg-1"
+                v-for="(link, index) in navLink"
+                :key="index"
+              >
+                <a
+                  :href="link.src"
+                  class="nav-link py-3 px-0 px-lg-3 text-dark"
+                >
+                  {{ link.label }}<i class="fas fa-chevron-down"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        
+        <!-- Icone social a destra -->
+        <div class="d-flex flex-row justify-content-center">
+          <ul class="navbar-nav mb-2 mb-lg-0">
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 text-dark" href="#">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 text-dark" href="#">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 text-dark" href="#">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </li>
+            <li class="nav-item mx-0 mx-lg-1">
+              <a class="nav-link py-3 px-0 px-lg-3 text-dark" href="#">
+                <i class="fab fa-linkedin"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+        </nav>
+      <!-- </div> -->
+    <!-- </div> -->
+  </div>
 </template>
-
 
 <script>
 export default {
-    name: "NavBar",
-    data() {
-        return {
-            navLink: [
-                {
-                    label: "Home",
-                    src: "#"
-                },
-                {
-                    label: "Pages",
-                    src: "#"
-                },
-                {
-                    label: "Courses",
-                    src: "#"
-                },
-                {
-                    label: "Features",
-                    src: "#"
-                },
-                {
-                    label: "Blog",
-                    src: "#"
-                },
-                {
-                    label: "Shop",
-                    src: "#"
-                },
-            ]
-            
-        }
-    }
-}
+  name: "NavBar",
+  data() {
+    return {
+      navLink: [
+        {
+          label: "Home",
+          src: "#",
+        },
+        {
+          label: "Pages",
+          src: "#",
+        },
+        {
+          label: "Courses",
+          src: "#",
+        },
+        {
+          label: "Features",
+          src: "#",
+        },
+        {
+          label: "Blog",
+          src: "#",
+        },
+        {
+          label: "Shop",
+          src: "#",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
-
+.logo img {
+  width: 8rem;
+}
 </style>
