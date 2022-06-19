b51b18292e75f0c4ed40034c4b1952d8d03f6d21
GiuliaV <valente.giulia93@gmail.com>
2022-06-19 20:32:23 +0200

    Modifica Style di quote e newsletter

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -2,16 +2,16 @@
   <!-- Main (8 sezioni) -->
   <main>
     <!-- Modalità corsi -->
-    <section class="container-fuid">
+    <section class="container-fuid ms_padding">
        <CoachComp /> 
     </section>
 
     <!-- Text -->
-       <section id="about" class="container-fluid">
+       <section id="about" class="container-fluid ms_padding">
            <div class="wrapper ">
                <div class="container">
                     <div class="row">
-                        <div class="col-xs-12 col-sm-6 text-center">
+                        <div class="col-xs-12 col-sm-6 text-center ms_text_bold">
                             <img src="@/assets/images/artist-quote-icon.png" alt="">
                             <p class="">
                                 I coach and mentor visual artists, like you, to Create Like You Mean It	
@@ -19,7 +19,7 @@
                             </p>
                             <p>Martin Garrix</p>
                         </div>
-                        <div class="col-xs-12 col-sm-6">
+                        <div class="col-xs-12 col-sm-6 align-items-center ms_text_normal">
                             <p class="text-left">
                                 As a coach and mentor, my ultimate goal is to motivate visual artists toward inspired action. I help you get you back on track to being the productive, creative, meaningful and authentic artist you truly want to be.I support artists in crafting and living a creative life as well as feeling good about themselves and their art.
                             </p>  
@@ -33,7 +33,7 @@
             </div>
         </section>
     <!-- Fake video -->
-    <section class="" id="ms_video">
+    <section class="ms_padding" id="ms_video">
         <VideoComp/>
 
     </section>
@@ -44,7 +44,7 @@
     </section>
 
     <!-- Corsi Online -->
-    <section class="">
+    <section class="ms_padding">
         <CourseComp/>
     </section>
 
@@ -55,19 +55,18 @@
     <section></section>
 
     <!-- Newsletter -->
-    <section id="newsletter" class="d-flex justify-content-center align-middle">
-      <div class="wrapper">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-xs-12 col-sm-6">
-              <p class="text-left">Newsletter</p>
+    <section id="newsletter" class="d-flex justify-content-center align-items-center ms_padding">
+      <div class="container">  
+          <div class="row ">
+            <div class="col-5 ms_text_news">
+              <p class="text-end ">Newsletter to get in touch</p>
             </div>
-            <div class="col-xs-12 col-sm-6">
-              <input type="email" placeholder="email" />
+            <div class="form__group field">
+              <input type="input" class="form__field" placeholder="Name" name="name" id='name' required />
+              <label for="name" class="form__label">Your email<i class="fa-solid fa-arrow-right ms_arrow"></i></label>
             </div>
           </div>
         </div>
-      </div>
     </section>
 
     <!-- Fine Main -->
@@ -92,6 +91,25 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/style/_commons.scss";
 @import "../assets/style/_variables.scss";
+#about{
+  height: 60%;
+  
+  .ms_text_bold{
+    padding: 0 7rem;
+    font-size: 40px;
+    font-weight: bold;
+  }
+  .ms_text_normal{
+    text-align: left;
+    padding: 4rem 5rem;
+    color: $Boulder;
+    p{
+      padding:1rem 0;
+    }
+
+
+  }
+}
 
 #newsletter {
   background-color: $BayofMany;
@@ -101,5 +119,69 @@ export default {
   background-position: right bottom, left bottom;
   background-repeat: no-repeat;
   background-size: contain;
+    .ms_text_news{
+      padding:0 8rem;
+      color: white;
+      p{
+      font-size: 35px;
+      }  
+    }
+    .form__group {
+  position: relative;
+  padding: 15px 0 0;
+  margin-top: 10px;
+  width: 50%;
+}
+
+.form__field {
+  font-family: inherit;
+  width: 80%;
+  border: 0;
+  border-bottom: 1px solid $DustGray;
+  outline: 0;
+  font-size: 1.3rem;
+  color: white;
+  padding: 7px 0;
+  background: transparent;
+  transition: border-color 0.2s;
+
+  &::placeholder {
+    color: transparent;
+  }
+
+  &:placeholder-shown ~ .form__label {
+    font-size: 1.3rem;
+    cursor: text;
+    top: 20px;
+  }
+}
+
+.form__label {
+  position: absolute;
+  top: 0;
+  display: block;
+  transition: 0.2s;
+  font-size: 1rem;
+  color: white;
+}
+
+.form__field:focus {
+  ~ .form__label {
+    position: absolute;
+    top: 0;
+    display: block;
+    transition: 0.2s;
+    font-size: 1rem;
+    color: white;
+    font-weight:700;    
+  }
+  padding-bottom: 6px;  
+  font-weight: 700;
+  border-width: 2px;
+  border-image-slice: 1;
+}
+.ms_arrow{
+  padding-left: 25rem;
+}
 }
 </style>
